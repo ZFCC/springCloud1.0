2604ceac612c534f92512faaf9117ce0fd73c372
ZFCC <719815832@qq.com>
2018-10-13 09:42:06 +0800

    springCloud eureka service build

--- src/main/java/com/sn/springCloud10/InitController.java
+++ src/main/java/com/sn/springCloud10/InitController.java
@@ -1,5 +1,6 @@
 package com.sn.springCloud10;
 
+import org.springframework.boot.json.JsonParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,9 +12,14 @@ public class InitController {
 
     @GetMapping("/hello")
     @ResponseBody
-    public String helloWord(){
-        String str = "today is interesting   \n"+new Date();
-        return str;
+    public User helloWord(){
+        User user = new User();
+        user.setAge(20);
+        user.setId("1");
+        user.setUserName("我是你好");
+        user.setPassWord("123");
+        System.out.println("today is interesting  \n"+new Date());
+        return user;
     }
 
 }
